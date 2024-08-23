b0165c4eab3945c0c1e303b2e06dfabf58edddac
ReyReyy <46741598+ReyReyy@users.noreply.github.com>
2024-08-24 01:12:14 +0800

    Add support for geoip

--- generate_rule_set.py
+++ generate_rule_set.py
@@ -3,17 +3,26 @@ import json
 import subprocess
 import shutil
 
-repo_url = "https://github.com/SagerNet/sing-geosite.git"
+site_url = "https://github.com/SagerNet/sing-geosite.git"
+ip_url = "https://github.com/SagerNet/sing-geoip.git"
 branch = "rule-set"
-local_dir = "/etc/sing-box/sing-geosite"
+site_dir = "/etc/sing-box/sing-geosite"
+ip_dir = "/etc/sing-box/sing-geoip"
 output_file = "/etc/sing-box/rule_set.json"
 
-if os.path.exists(local_dir):
-    # print(f"Deleted {local_dir}...")
-    shutil.rmtree(local_dir)
+if os.path.exists(site_dir):
+    # print(f"Deleted {site_dir}...")
+    shutil.rmtree(site_dir)
 
-print(f"git cloning {repo_url} ...")
-subprocess.run(["git", "clone", "-b", branch, repo_url, local_dir])
+if os.path.exists(ip_dir):
+    # print(f"Deleted {ip_dir}...")
+    shutil.rmtree(ip_dir)
+
+print(f"git cloning {site_url} ...")
+subprocess.run(["git", "clone", "-b", branch, site_url, site_dir])
+
+print(f"git cloning {ip_url} ...")
+subprocess.run(["git", "clone", "-b", branch, ip_url, ip_dir])
 
 config = {
     "route": {
@@ -21,14 +30,24 @@ config = {
     }
 }
 
-for filename in os.listdir(local_dir):
+for filename in os.listdir(site_dir):
     if filename.startswith("geosite-") and filename.endswith(".srs"):
         tag = filename.replace(".srs", "")
         config["route"]["rule_set"].append({
             "tag": tag,
             "type": "local",
             "format": "binary",
-            "path": os.path.join(local_dir, filename)
+            "path": os.path.join(site_dir, filename)
+        })
+
+for filename in os.listdir(ip_dir):
+    if filename.startswith("geoip-") and filename.endswith(".srs"):
+        tag = filename.replace(".srs", "")
+        config["route"]["rule_set"].append({
+            "tag": tag,
+            "type": "local",
+            "format": "binary",
+            "path": os.path.join(ip_dir, filename)
         })
 
 config_json = json.dumps(config, indent=2)
